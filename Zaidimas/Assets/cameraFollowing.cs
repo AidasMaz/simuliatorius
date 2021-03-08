597e35c32de922b1b99da3291c686abb0d9f2799
AidasMaz <aidas0505@gmail.com>
2021-03-08 23:48:37 +0200

    Added colliders and smooth camera following

---
+++ Zaidimas/Assets/cameraFollowing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowing : MonoBehaviour
+{
+    [Header("Follow targer (Player)")]
+    public Transform target;
+    [Range(0.01f, 30f)]
+    public float smoothSpeed = 5f;
+
+    //-------------------------------
+
+    void FixedUpdate()
+    {
+        Vector2 desiredPosition = new Vector2(target.position.x, target.position.y);
+        Vector2 smoothedPosition = Vector2.Lerp(new Vector2(transform.position.x, transform.position.y ), desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
+    }
+}
