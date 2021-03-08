e5b25e7a45881201f0a691ea2c1355ca88edc703
AidasMaz <aidas0505@gmail.com>
2021-03-08 17:34:39 +0200

    Added basic movement + separated floor from walls

---
+++ Zaidimas/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement speed")]
+    public float movementSpeed = 5f;
+    [Header("Rigid body")]
+    public Rigidbody2D rigidbody;
+    [Space]
+    private Vector2 movement;
+
+    //--------------------------------
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
