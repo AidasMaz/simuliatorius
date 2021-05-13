6238be31bae31b745206d0eb9c719b10c0c68d11
AidasMaz <aidas0505@gmail.com>
2021-05-14 00:55:26 +0300

    Small set ups for game (work place grid, item pulsing example)

---
+++ Zaidimas/Assets/Scripts/ItemColorPulsing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemColorPulsing : MonoBehaviour
+{
+    [Header("Colors")]
+    public Color PulsingColor;
+    [Space]
+    public bool pulse;
+
+    //+++++++++++++++++++++++++++++++++++++++++++
+
+    private void Start()
+    {
+        pulse = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (pulse)
+        {
+            this.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, PulsingColor, Mathf.PingPong(Time.time, 1));
+        }
+    }
+}
